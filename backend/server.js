6e6602cead06bc670c0cfda54a62706922b324e7
root <root@ip-172-31-36-2.ap-south-1.compute.internal>
2026-03-16 11:37:42 +0000

    fix cors, mongodb url and api url

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// parse requests of content-type - application/json
+app.use(cors());
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
@@ -23,14 +21,12 @@ db.mongoose
     process.exit();
   });
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
